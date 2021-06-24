5e04dc1c56b60f99b81fb4d2a7d6277ba7e6030f
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-06-24 18:50:58 -0400

    successfully used a get put in index.js

--- index.js
+++ index.js
@@ -27,8 +27,16 @@ app.get('/songbyId', (req, res) =>{
 })
 
 
-app.post('/api/products', (req, res) => {
+app.post('/api/addsong', (req, res) => {
     const newSong = req.body;
     const addedSong = repoContext.songs.createSong(newSong);
     return res.send(addedSong);
+})
+
+app.put('/api/updatesong', (req, res) =>{
+    const id = req.params.id;
+    const songPropertiesToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+ return res.send(updatedSong)
+
 })
